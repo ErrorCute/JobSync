e88b7f9c482f8648faf64090accbcb8c8d89c1ef
Victor <vict.sepulveda@duocuc.cl>
2024-06-20 15:11:30 -0400

    bd

--- JobSync/JobSyncApp/urls.py
+++ JobSync/JobSyncApp/urls.py
@@ -37,11 +37,8 @@ urlpatterns = [
     path('reagendar_trabajo/<int:trabajo_id>/', views_colaborador.reagendar_trabajo, name='reagendar_trabajo'),
     path('actualizar_estado_trabajo/<int:trabajo_id>', views_colaborador.actualizar_estado_trabajo, name='actualizar_estado_trabajo'),
     # reportes
-<<<<<<< HEAD
     # ---vista admin
     path('index_reporte/', views_reporte.index_reporte, name='index_reporte'),
-=======
->>>>>>> rama-eliminar
     path('reporte_general/', views_reporte.reporte_general, name='reporte_general'),
     path('selecciona_colaborador_reporte/', views_reporte.selecciona_colaborador_reporte, name='selecciona_colaborador_reporte'),
     path('reporte_colaborador/<int:colaborador_id>/', views_reporte.reporte_colaborador, name='reporte_colaborador'),
